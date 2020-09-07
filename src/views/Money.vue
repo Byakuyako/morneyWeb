8a54cced3f9af2b79f95db67f3a17c8f2e4ab25c
Jesse <dynaptx@163.com>
2020-09-07 21:37:59 +0800

    Money 页面布局

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,5 +1,26 @@
 <template>
-  <div>Money.vue
+  <div class="nav-wrapper">
+    <div class="content">
+      <p>Money.vue</p>
+      <p>Money.vue</p>
+      <p>Money.vue</p>
+      <p>Money.vue</p>
+      <p>Money.vue</p>
+      <p>Money.vue</p>
+      <p>Money.vue</p>
+      <p>Money.vue</p>
+      <p>Money.vue</p>
+      <p>Money.vue</p>
+      <p>Money.vue</p>
+      <p>Money.vue</p>
+      <p>Money.vue</p>
+      <p>Money.vue</p>
+      <p>Money.vue</p>
+      <p>Money.vue</p>
+      <p>Money.vue</p>
+      <p>Money.vue</p>
+
+    </div>
     <Nav/>
   </div>
 </template>
@@ -11,5 +32,15 @@
 </script>
 
 <style lang="scss" scoped>
-
+  .nav-wrapper {
+    border: 1px solid peru;
+    display: flex;
+    flex-direction: column;
+    height: 100vh;
+  }
+  .content {
+    border: 1px solid blue;
+    overflow: auto;
+    flex-grow: 1; //尽量不更多的空间给content
+  }
 </style>
