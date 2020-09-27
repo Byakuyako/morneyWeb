47b1ea35e0c0456796b851faff15b53ef208a39b
Jesse <dynaptx@163.com>
2020-09-27 23:00:13 +0800

    封装一个 Button 组件

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -5,7 +5,7 @@
 
       <Types :value.sync="record.type"/>
 
-      <Notes field-name="备注" placeholder="在这里输入备注" @update:value="onUpdateNotes"/>
+      <FormItem field-name="备注" placeholder="在这里输入备注" @update:value="onUpdateNotes"/>
       <!--      .sync很关键-->
       <Tags :data-source.sync="tags" @update:value="onUpdateTags"/>
 
@@ -16,29 +16,29 @@
 <script lang="ts">
   import NumberPad from '@/components/Money/NumberPad.vue';
   import Types from '@/components/Money/Types.vue';
-  import Notes from '@/components/Money/Notes.vue';
+  import FormItem from '@/components/Money/FormItem.vue';
   import Tags from '@/components/Money/Tags.vue';
   import Vue from 'vue';
   import {Component, Watch} from 'vue-property-decorator';
-  import model from '@/models/tagListModel';
+  import recordListModel from '@/models/tagListModel';
   import tagListModel from '@/models/tagListModel';
+
   // import model from '@/model';
   // const model = require('@/model.js').default;
   // console.log(model);
 
   // const version = window.localStorage.getItem('version') || '0';
 
-  const recordList = model.fetch();
+  const recordList = recordListModel.fetch();
   const tagList = tagListModel.fetch()
 
 
 
   @Component({
-    components: {Tags, Notes, Types, NumberPad}
+    components: {Tags, FormItem, Types, NumberPad}
   })
   export default class Money extends Vue {
     tags = tagList;
-    // recordList: Record[] = model.fetch()
     recordList: RecordItem[] = recordList;
     record: RecordItem = {
       tags: [],
@@ -63,7 +63,7 @@
     }
 
     saveRecord() {
-      const record2 = model.clone(this.record);
+      const record2 = recordListModel.clone(this.record);
       record2.createdAt = new Date();
       this.recordList.push(record2);
       console.log(this.recordList);
@@ -73,7 +73,7 @@
     onRecordListChanged() {
       //转成字符串存到localStorage
       // window.localStorage.setItem('recordList', JSON.stringify(this.recordList));
-      model.save(this.recordList);
+      recordListModel.save(this.recordList);
     }
   }
 </script>
